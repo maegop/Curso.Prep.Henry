b351d783681f85bc780dd1217f8311d7c5271bf8
Mario Gordon <80539604+maegop@users.noreply.github.com>
2021-11-21 05:03:09 -0500

    ejercicios hasta el 3

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -6,6 +6,10 @@ function crearGato(nombre, edad) {
   // Agrega un método (funcion) llamado "meow" que devuelva el string "Meow!"
   // Devuelve el objeto
   // Tu código:
+
+  return (
+    {nombre: nombre, edad: edad, meow: function(){return "Meow!"}}
+  )
 }
 
 
@@ -14,6 +18,7 @@ function agregarPropiedad(objeto, property) {
   // Devuelve el objeto
   // NOTA: El nombre de la propiedad no es "propiedad", el nombre es el valor del argumento llamado "property" (una cadena/string)
   // Tu código:
+  // return({objeto: objeto, property: property})
 }
 
 function invocarMetodo(objeto, metodo) {
@@ -21,13 +26,14 @@ function invocarMetodo(objeto, metodo) {
   // Invoca ese método
   // Nada necesita ser devuelto ("returned")
   // Tu código:
+ // {objeto: metodo()}
 }
 
 function multiplicarNumeroDesconocidoPorCinco(objetoMisterioso) {
   // "objetoMisterioso" tiene una propiedad llamada "numeroMisterioso"
   // Multiplica el numeroMisterioso por 5 y devuelve el producto
   // Tu código:
-
+  return objetoMisterioso.numeroMisterioso *5
 }
 
 function eliminarPropiedad(objeto, unaPropiedad) {
